775883ee4fe9be56d412c928eda77f838b3d32c8
nepa27 <nepalek@mail.ru>
2026-03-16 15:06:52 +0300

    Add base operation of CRUD

---
+++ src/base/crud_base.py
@@ -0,0 +1,97 @@
+from typing import Type, TypeVar, Sequence, Any
+
+from sqlalchemy import select, update
+from sqlalchemy.ext.asyncio import AsyncEngine, AsyncSession
+from sqlalchemy.sql import text
+
+from src.base.models import Base
+from src.config.decorators import logger
+
+
+ModelType = TypeVar('ModelType')
+CreateSchemaType = TypeVar('CreateSchemaType')
+UpdateSchemaType = TypeVar('UpdateSchemaType')
+
+
+class CRUDBase:
+    """Универсальный базовый класс для CRUD операций."""
+
+    def __init__(self, model: Type[Base]):
+        """
+        Инициализирует CRUD-класс с указанной моделью.
+
+        Параметры:
+            model: SQLAlchemy-модель (класс), связанный с таблицей в БД.
+        """
+        self.model = model
+
+    async def get_all(self, session: AsyncSession) -> Sequence[Any]:
+        """Получает данные из БД."""
+        query = await session.execute(select(self.model))
+        return query.scalars().all()
+
+    async def get_one_or_none(self, obj_id: int, session: AsyncSession) -> ModelType | None:
+        """Получает объект из БД по id или выбрасывает 404-ошибку."""
+        query = await session.execute(
+            select(self.model).where(self.model.id == obj_id)
+        )
+        return query.scalar_one_or_none()
+
+    async def create(self, data: CreateSchemaType, session: AsyncSession) -> ModelType:
+        """Добавляет данные в БД."""
+        db_obj = self.model(**data)
+        try:
+            session.add(db_obj)
+            await session.commit()
+            await session.refresh(db_obj)
+
+            return db_obj
+        except Exception as e:
+            logger.error(f'Ошибка при добавлении данных в БД: {e}')
+            await session.rollback()
+
+    async def update(self, data: dict, obj_id: int, session: AsyncSession) -> None:
+        """Обновляет данные в БД."""
+        try:
+            query = (
+                update(self.model)
+                .values(**data)
+                .filter_by(id=obj_id)
+            )
+            await session.execute(query)
+            await session.commit()
+        except Exception as e:
+            logger.error(f'Ошибка при обновлении данных в БД: {e}')
+            await session.rollback()
+
+    @staticmethod
+    async def delete(db_object: Base, session: AsyncSession) -> None:
+        """Удаляет данные из БД."""
+        try:
+            await session.delete(db_object)
+            await session.commit()
+        except Exception as e:
+            logger.error(f'Ошибка при удалении данных в БД: {e}')
+            await session.rollback()
+
+    @staticmethod
+    async def check_db_connection(engine: AsyncEngine) -> None:
+        try:
+            async with engine.connect() as connection:
+                result = await connection.execute(text('SELECT version();'))
+                db_version = result.scalar_one()
+                logger.info(f'База данных подключена. Версия: {db_version}')
+        except Exception as e:
+            logger.info(f'Ошибка при подключении к БД: {e}')
+
+    @staticmethod
+    async def create_tables(engine: AsyncEngine) -> None:
+        """Функция создания таблиц."""
+        async with engine.begin() as conn:
+            await conn.run_sync(Base.metadata.create_all)
+
+    @staticmethod
+    async def delete_tables(engine: AsyncEngine) -> None:
+        """Функция удаления таблиц."""
+        async with engine.begin() as conn:
+            await conn.run_sync(Base.metadata.drop_all)
