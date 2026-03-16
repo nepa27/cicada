32e80c2e5aaf6e52ddceeb0e01564d9c47aa9b4b
nepa27 <nepalek@mail.ru>
2026-03-16 14:16:22 +0300

    Add decorator for register time of work

---
+++ src/config/decorators.py
@@ -0,0 +1,38 @@
+import sys
+from logging.handlers import RotatingFileHandler
+
+import logging
+from functools import wraps
+from typing import Callable
+
+
+log_format = (
+    '%(asctime)s - [%(levelname)s] -  %(name)s - '
+    '(%(filename)s).%(funcName)s(%(lineno)d) - %(message)s'
+)
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+stream_handler = logging.StreamHandler(stream=sys.stdout)
+stream_handler.setFormatter(logging.Formatter(log_format))
+
+file_handler = RotatingFileHandler(
+    'time_tracker.log',
+    maxBytes=5 * 1024 * 1024,
+    backupCount=5,
+    encoding='UTF-8',
+)
+file_handler.setFormatter(logging.Formatter(log_format))
+
+logger.addHandler(stream_handler)
+logger.addHandler(file_handler)
+
+
+def logging_decorator(func: Callable) -> Callable:
+    """Логирует результат выполнения функции."""
+    @wraps(func)
+    def wrapper(*args, **kwargs) -> Callable:
+        result = func(*args, **kwargs)
+        logger.info(f'Отработала функция: {func.__name__}')
+        return result
+    return wrapper
