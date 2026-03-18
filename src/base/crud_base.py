2fa56ca7eec081bc8acbe15d036d448476a8eab5
nepa27 <nepalek@mail.ru>
2026-03-18 16:45:56 +0300

    Add session to params of class

--- src/base/crud_base.py
+++ src/base/crud_base.py
@@ -16,7 +16,7 @@ UpdateSchemaType = TypeVar('UpdateSchemaType')
 class CRUDBase:
     """Универсальный базовый класс для CRUD операций."""
 
-    def __init__(self, model: Type[Base]):
+    def __init__(self, model: Type[Base], session: AsyncSession):
         """
         Инициализирует CRUD-класс с указанной моделью.
 
@@ -24,33 +24,34 @@ class CRUDBase:
             model: SQLAlchemy-модель (класс), связанный с таблицей в БД.
         """
         self.model = model
+        self.session = session
 
-    async def get_all(self, session: AsyncSession) -> Sequence[Any]:
+    async def get_all(self) -> Sequence[Any]:
         """Получает данные из БД."""
-        query = await session.execute(select(self.model))
+        query = await self.session.execute(select(self.model))
         return query.scalars().all()
 
-    async def get_one_or_none(self, obj_id: int, session: AsyncSession) -> ModelType | None:
+    async def get_one_or_none(self, obj_id: int) -> ModelType | None:
         """Получает объект из БД по id или выбрасывает 404-ошибку."""
-        query = await session.execute(
+        query = await self.session.execute(
             select(self.model).where(self.model.id == obj_id)
         )
         return query.scalar_one_or_none()
 
-    async def create(self, data: CreateSchemaType, session: AsyncSession) -> ModelType:
+    async def create(self, data: CreateSchemaType) -> ModelType:
         """Добавляет данные в БД."""
         db_obj = self.model(**data)
         try:
-            session.add(db_obj)
-            await session.commit()
-            await session.refresh(db_obj)
+            self.session.add(db_obj)
+            await self.session.commit()
+            await self.session.refresh(db_obj)
 
             return db_obj
         except Exception as e:
             logger.error(f'Ошибка при добавлении данных в БД: {e}')
-            await session.rollback()
+            await self.session.rollback()
 
-    async def update(self, data: dict, obj_id: int, session: AsyncSession) -> None:
+    async def update(self, data: dict, obj_id: int) -> None:
         """Обновляет данные в БД."""
         try:
             query = (
@@ -58,11 +59,11 @@ class CRUDBase:
                 .values(**data)
                 .filter_by(id=obj_id)
             )
-            await session.execute(query)
-            await session.commit()
+            await self.session.execute(query)
+            await self.session.commit()
         except Exception as e:
             logger.error(f'Ошибка при обновлении данных в БД: {e}')
-            await session.rollback()
+            await self.session.rollback()
 
     @staticmethod
     async def delete(db_object: Base, session: AsyncSession) -> None:
